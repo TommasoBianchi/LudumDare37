bb6b32ee817b2c8f5cd4e9241bce874fd0c00e58
Tommaso <tommaso4.bianchi@mail.polimi.it>
2016-12-10 12:59:23 +0100

    Animations for goblin and main character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,13 @@ public class PlayerController : MonoBehaviour {
     public float Damage;
 
     private int MaxVita;
+    private Animator animator;
      
 	// Use this for initialization
 	void Start () {
         Damage = 0;
         WeaponType = WeaponTypes.Sword;
+        animator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -25,6 +27,34 @@ public class PlayerController : MonoBehaviour {
         Vector2 Movement = new Vector2(MoveHorizontal, MoveVertical);
 
         transform.Translate(Movement * Speed * Time.deltaTime, Space.World);
-		
+
+        UpdateAnimator(Movement);
 	}
+
+    private void UpdateAnimator(Vector2 Movement)
+    {
+        animator.SetFloat("AnimationSpeed", Speed);
+
+        if (Movement.sqrMagnitude > 0)
+        {
+            if (Mathf.Abs(Movement.x) > Mathf.Abs(Movement.y))
+            {
+                if (Movement.x > 0)
+                    animator.SetInteger("Direction", 1); // right
+                else
+                    animator.SetInteger("Direction", 3); // left
+            }
+            else
+            {
+                if (Movement.y > 0)
+                    animator.SetInteger("Direction", 2); // back
+                else
+                    animator.SetInteger("Direction", 0); // front
+            }
+        }
+        else
+        {
+            animator.SetInteger("Direction", -1); // idle
+        }
+    }
 }
