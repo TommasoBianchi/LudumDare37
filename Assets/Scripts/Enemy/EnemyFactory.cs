7fc232384f975bc2766ae242b4a34be5e56711f6
Tommaso <tommaso4.bianchi@mail.polimi.it>
2016-12-10 18:54:57 +0100

    RoomPlanFactory

--- Assets/Scripts/Enemy/EnemyFactory.cs
+++ Assets/Scripts/Enemy/EnemyFactory.cs
@@ -8,10 +8,12 @@ public class EnemyFactory : MonoBehaviour {
 	public static List<Enemy> enemies;
 	public static List<Enemy> bosses;
 
-	public static Enemy getEnemy(int life) {
+    public static Enemy getEnemy(int roomID, float lifeModifier)
+    {
 		Enemy enemy = getRandomEnemy();
 
-		enemy.Life = life;
+        Weapon currentPlayerWeapon = Globals.GetPlayerController().Weapon;
+        enemy.Life = Mathf.RoundToInt((27 + (currentPlayerWeapon.Tier * 5)) * 5 / (dmg_calc(roomID, currentPlayerWeapon, enemy)) * lifeModifier); ;
 		addWeaknessesAndResistences(enemy);
 		enemy.PowerUp = PowerUpFactory.GetRandomPowerUp();
 		enemy.Scale = Random.Range(Constants.ENEMY_MIN_SCALE, Constants.ENEMY_MAX_SCALE);
@@ -45,10 +47,11 @@ public class EnemyFactory : MonoBehaviour {
 		));
 	}
 
-	public static Enemy getBoss(int life) {
+	public static Enemy getBoss(int roomID, float lifeModifier) {
 		Enemy boss = getRandomBoss();
 
-		boss.Life = life;
+        Weapon currentPlayerWeapon = Globals.GetPlayerController().Weapon;
+        boss.Life = Mathf.RoundToInt((27 + (currentPlayerWeapon.Tier * 5)) * 5 / (dmg_calc(roomID, currentPlayerWeapon, boss)) * lifeModifier);
 		addWeaknessesAndResistences(boss);
 		boss.PowerUp = PowerUpFactory.GetRandomPowerUp();
 		boss.Scale = Random.Range(Constants.ENEMY_MIN_SCALE, Constants.ENEMY_MAX_SCALE);
@@ -61,4 +64,48 @@ public class EnemyFactory : MonoBehaviour {
 		int bossIndex = Random.Range(0, bosses.Count);
 		return bosses[bossIndex];
 	}
+    private static float dmg_calc(int liv, Weapon arma, Enemy enemy)
+    {
+        float dmg_modif;
+        int oldupper = 0;
+        int upper = 0;
+        int lower = 0;
+        int i = 0;
+        for (i = 1; i <= arma.Tier; i++)
+        {
+            upper += (10 + (i - 1) * 2);
+            lower = oldupper;
+            oldupper = upper;
+        }
+
+        dmg_modif = (-0.5f / (upper - lower) * (liv - lower) + 0.25f) + 1;
+        if (dmg_modif <= 0.01f)
+            dmg_modif = 0.01f;
+
+        switch (arma.Roll)
+        {
+            case Roll.Strong:
+                dmg_modif = dmg_modif * (1);
+                break;
+            case Roll.Audacious:
+                dmg_modif = dmg_modif * (1 + 0.22f);
+                break;
+            case Roll.Cruel:
+                dmg_modif = dmg_modif * (1 - 0.18f);
+                break;
+            default:
+                break;
+        }
+
+        if (enemy.Weaknesses.Contains(arma.Type))
+        {
+            dmg_modif = dmg_modif * (1 + 0.30f);
+        }
+        else
+        {
+            dmg_modif = dmg_modif * (1 - 0.12f);
+        }
+
+        return dmg_modif;
+    }
 }
--- Assets/Scripts/Room/RoomPlanFactory.cs
+++ Assets/Scripts/Room/RoomPlanFactory.cs
@@ -19,7 +19,6 @@ public class RoomPlanFactory : MonoBehaviour {
         List<Resource> loot = new List<Resource>();
 
         int dropAmount = Random.Range(3, 7);
-        //int life = (27 + (tier * 5)) * 5 / (dmg_calc)
 
         bool bossRoom = Random.Range(0, 5) == 0;
         if (bossRoom)
@@ -27,18 +26,28 @@ public class RoomPlanFactory : MonoBehaviour {
             dropAmount = Mathf.RoundToInt(dropAmount * 1.20f);
 
             List<Enemy> enemies = new List<Enemy>();
-            enemies.Add(EnemyFactory.getBoss(0));
+            enemies.Add(EnemyFactory.getBoss(roomID, Random.Range(4f, 6f)));
 
             bursts.Add(new Burst(0, enemies, new Vector2(0.5f, 0.5f)));
         }
         else
         {
             int numberOfEnemies = Random.Range(3, 5);
+            int numberOfBursts = 2;
+            int numberOfEnemiesPerBurst = numberOfEnemies / numberOfBursts;
 
-            List<Enemy> enemies = new List<Enemy>();
-            for (int i = 0; i < numberOfEnemies / 2; i++)
+            for (int b = 0; b < numberOfBursts; b++)
             {
-                enemies.Add(EnemyFactory.getEnemy(50));
+                if (b == numberOfBursts - 1)
+                    numberOfEnemiesPerBurst = numberOfEnemies;
+
+                List<Enemy> enemies = new List<Enemy>();
+                for (int i = 0; i < numberOfEnemiesPerBurst; i++)
+                {
+                    enemies.Add(EnemyFactory.getEnemy(roomID, Random.Range(0.9f, 1.1f)));
+                }
+                numberOfEnemies -= numberOfEnemiesPerBurst;
+                bursts.Add(new Burst(b * Random.Range(0.5f, 2.5f), enemies, Random.insideUnitCircle));
             }
         }
 
@@ -55,50 +64,5 @@ public class RoomPlanFactory : MonoBehaviour {
         }
 
         return new RoomPlan(bursts, loot);
-	}
-
-    float dmg_calc(int liv, Weapon arma, Enemy enemy, Roll roll)
-    {
-        float dmg_modif;
-        int oldupper = 0;
-        int upper = 0;
-        int lower = 0;
-        int i = 0;
-        for (i = 1; i <= arma.Tier; i++)
-        {
-            upper += (10 + (i - 1) * 2);
-            lower = oldupper;
-            oldupper = upper;
-        }
-
-        dmg_modif = (-0.5f / (upper - lower) * (liv - lower) + 0.25f) + 1;
-        if (dmg_modif <= 0.01f)
-            dmg_modif = 0.01f;
-
-        switch (roll)
-        {
-            case Roll.Strong:
-                dmg_modif = dmg_modif * (1);
-                break;
-            case Roll.Audacious:
-                dmg_modif = dmg_modif * (1 + 0.22f);
-                break;
-            case Roll.Cruel:
-                dmg_modif = dmg_modif * (1 - 0.18f);
-                break;
-            default:
-                break;
-        }
-
-        if (enemy.Weaknesses.Contains(arma.Type))
-        {
-            dmg_modif = dmg_modif * (1 + 0.30f);
-        }
-        else
-        {
-            dmg_modif = dmg_modif * (1 - 0.12f);
-        }
-
-        return dmg_modif;
-    }
+	}    
 }
