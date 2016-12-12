0fd0f03c26275796350de1cd5bc45c69313f9877
Fabio Chiusano <chiusanofabio@hotmail.it>
2016-12-12 16:00:26 +0100

    fixed anti geco

--- Assets/Scripts/Room/ExternalWall.cs
+++ Assets/Scripts/Room/ExternalWall.cs
@@ -9,7 +9,7 @@ public class ExternalWall : MonoBehaviour {
 	private bool spawned = false;
 
 	void OnTriggerEnter2D(Collider2D collider) {
-		if (!spawned) {
+		if (collider.gameObject.layer == 10 && !spawned) { // if player
 			Vector3 pos = new Vector3(Globals.GetPlayer().transform.position.x + 30, Globals.GetPlayer().transform.position.y + 30, Globals.GetPlayer().transform.position.z);
 			GameObject go = Instantiate(enemy, pos, Quaternion.identity);
 			go.GetComponent<MovementAI>().target = Globals.GetPlayer().GetComponent<Rigidbody2D>();
